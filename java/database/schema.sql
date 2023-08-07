923256e15ecb2e8a9ff597bda73811fbcfb48ec5
Marina Sanchez[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C <marina8829@gmail.com>
2023-08-07 13:33:02 -0700

    Added pizza table to schema.sql

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS pizza, users;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -8,6 +8,18 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE pizza (
+    pizza_id SERIAL PRIMARY KEY,
+    --sale_id int REFERENCES sale(sale_id),
+    size varchar(10) DEFAULT('Large'),
+    pizza_name varchar(50) NOT NULL UNIQUE,
+    sauce varchar(10),
+    crust varchar(10) DEFAULT('Hand Tossed'),
+    price numeric(4,2) NOT NULL,
+    additional_instructions varchar(500),
+    is_visible boolean NOT NULL DEFAULT (TRUE)
 );
 
 COMMIT TRANSACTION;
