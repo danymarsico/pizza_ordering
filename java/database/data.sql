384e289184c756be99898a6275da77858fa07851
Anastassia Bowers <anastassia.bowers@gmail.com>
2023-08-08 12:23:49 -0700

    Updated schema + fixed errors

--- java/database/data.sql
+++ java/database/data.sql
@@ -1,17 +1,18 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
-INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
-INSERT INTO users (username,password_hash,role) VALUES ('test_user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
-
-INSERT INTO customer (first_name,last_name,street_address,city,phone_number,user_id) VALUES ('Test First','Test Last','123 Test Street','Test City', '123-456-7890','3');
-
-INSERT INTO sale (total,is_delivery,customer_id) VALUES ('420.00','TRUE','1');
-
-INSERT INTO pizza (sale_id,size,pizza_name,sauce,crust,price,additional_instructions,is_visible) VALUES ('1',DEFAULT,'The Butcher','Marinara',DEFAULT,'30.00','Test instructions','TRUE');
-
-INSERT INTO topping (topping_name,additional_price,is_visible,is_premium) VALUES ('pepperoni','1.25','TRUE','FALSE');
-
-INSERT INTO pizza_topping (pizza_id,topping_name) VALUES ('1','pepperoni');
+--INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+--INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+--INSERT INTO users (username,password_hash,role) VALUES ('test_user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+--
+--INSERT INTO customer (first_name,last_name,street_address,city,phone_number,user_id) VALUES ('Test First','Test Last','123 Test Street','Test City', '123-456-7890','3');
+--
+--INSERT INTO sale (total,is_delivery,customer_id) VALUES ('420.00','TRUE','1');
+--
+--INSERT INTO pizza (sale_id,size,pizza_name,sauce,crust,price,additional_instructions,is_visible) VALUES ('1',DEFAULT,'The Butcher','Marinara',DEFAULT,'30.00','Test instructions','TRUE');
+--
+--INSERT INTO topping (topping_name,additional_price,is_visible,is_premium) VALUES ('pepperoni','1.25','TRUE','FALSE');
+--
+--INSERT INTO pizza_topping (pizza_id,topping_name) VALUES ('1','pepperoni');
 
 COMMIT TRANSACTION;
+
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS specialty_topping, pizza_topping, topping, current_pizza, specialty_pizza, order, customer, users;
+DROP TABLE IF EXISTS specialty_topping, pizza_topping, topping, current_pizza, specialty_pizza, customer_order, customer, users;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -20,7 +20,7 @@ CREATE TABLE customer (
     user_id int NOT NULL REFERENCES users(user_id)
 );
 
-CREATE TABLE order (
+CREATE TABLE customer_order (
     order_id SERIAL PRIMARY KEY,
     total numeric(6,2),
     is_delivery boolean NOT NULL,
@@ -37,14 +37,14 @@ CREATE TABLE specialty_pizza (
 
 CREATE TABLE current_pizza (
     pizza_id SERIAL PRIMARY KEY,
-    order_id int NOT NULL REFERENCES sale(sale_id),
+    order_id int NOT NULL REFERENCES customer_order(order_id),
     size varchar(10) DEFAULT('Large') NOT NULL ,
     pizza_name varchar(50) NOT NULL UNIQUE,
     sauce varchar(10),
     crust varchar(15) DEFAULT('Hand Tossed') NOT NULL,
     price numeric(4,2) NOT NULL,
     specialty_id int NOT NULL REFERENCES specialty_pizza(specialty_id),
-    additional_instructions varchar(500),
+    additional_instructions varchar(500)
 );
 
 CREATE TABLE topping (
@@ -55,7 +55,7 @@ CREATE TABLE topping (
 );
 
 CREATE TABLE pizza_topping (
-    pizza_id int NOT NULL REFERENCES pizza(pizza_id),
+    pizza_id int NOT NULL REFERENCES current_pizza(pizza_id),
     topping_name varchar(15) NOT NULL REFERENCES topping(topping_name),
     CONSTRAINT pk_pizza_topping PRIMARY KEY (pizza_id, topping_name)
 );
