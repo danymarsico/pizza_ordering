d0a53a5cafdb56d0ca6d9b6fb31058734238b0ee
Daniele Marsico <danymarsico@gmail.com>
2023-08-10 13:02:38 -0400

    Refreshing of the database

--- java/database/data.sql
+++ java/database/data.sql
@@ -6,20 +6,64 @@ INSERT INTO users (username,password_hash,role) VALUES ('test_user','$2a$08$UkVv
 
 INSERT INTO customer (first_name,last_name,street_address,city,phone_number,user_id) VALUES ('Test First','Test Last','123 Test Street','Test City', '123-456-7890',3);
 
-INSERT INTO customer_order (total,is_delivery,date, is_active,customer_id) VALUES ( 420.00,'TRUE','2023-07-26','TRUE',1);
+INSERT INTO size (size_name, size_price) VALUES ('SMALL', 10.00);
+INSERT INTO size (size_name, size_price) VALUES ('MEDIUM', 14.00);
+INSERT INTO size (size_name, size_price) VALUES ('LARGE', 18.00);
 
-INSERT INTO specialty_pizza (price,pizza_name) VALUES ( 20.00,'The Butcher');
-INSERT INTO specialty_pizza (price,pizza_name) VALUES ( 18.00,'The G.O.A.T.');
+INSERT INTO crust (crust_name, crust_price) VALUES ('Hand Tossed', 0.00);
+INSERT INTO crust (crust_name, crust_price) VALUES ('Thin', 0.00);
+INSERT INTO crust (crust_name, crust_price) VALUES ('Pan', 1.00);
+INSERT INTO crust (crust_name, crust_price) VALUES ('Stuffed', 2.00);
 
-INSERT INTO current_pizza (order_id,size,pizza_name,sauce,crust,price,specialty_id,additional_instructions) VALUES (1,DEFAULT,'The Butcher','Marinara',DEFAULT,20.00,1,'Please do not add onions');
+INSERT INTO sauce (sauce_name, sauce_price) VALUES ('None', 0.00);
+INSERT INTO sauce (sauce_name, sauce_price) VALUES ('Tomato', 0.00);
+INSERT INTO sauce (sauce_name, sauce_price) VALUES ('Pesto', 0.75);
+INSERT INTO sauce (sauce_name, sauce_price) VALUES ('Buffalo', 0.75);
+INSERT INTO sauce (sauce_name, sauce_price) VALUES ('White Garlic', 0.75);
+
+INSERT INTO specialty_pizza (pizza_name, pizza_desc) VALUES ('The Butcher', '');
+INSERT INTO specialty_pizza (pizza_name, pizza_desc) VALUES ('The G.O.A.T.', '');
+INSERT INTO specialty_pizza (pizza_name, pizza_desc) VALUES ('OG NAMELESS', '');
+INSERT INTO specialty_pizza (pizza_name, pizza_desc) VALUES ('Playlist Deluxe', '');
+
+INSERT INTO customer_order (total, is_delivery, date, customer_id) VALUES (75.00,TRUE,'2023-08-09', 1);
+INSERT INTO customer_order (total,is_delivery,date,customer_id) VALUES ( 420.00,TRUE,'2023-07-26',1);
+
+
+INSERT INTO pizza (additional_instructions, size_name, sauce_name, crust_name, order_id, price, order_status, specialty_id) VALUES ('Instructions',DEFAULT,'Tomato',DEFAULT,1,20.00,DEFAULT, NULL);
+INSERT INTO pizza (additional_instructions, size_name, sauce_name, crust_name, order_id, price, order_status, specialty_id) VALUES ('Instructions',DEFAULT,'Tomato',DEFAULT,1,14.00,DEFAULT, NULL);
 
 INSERT INTO topping (topping_name,additional_price,is_premium) VALUES ('pepperoni',1.25,FALSE);
+INSERT INTO topping (topping_name,additional_price,is_premium) VALUES ('sausage',1.25,FALSE);
+INSERT INTO topping (topping_name,additional_price,is_premium) VALUES ('ham',1.25,FALSE);
+INSERT INTO topping (topping_name,additional_price,is_premium) VALUES ('olive',1.25,FALSE);
+INSERT INTO topping (topping_name,additional_price,is_premium) VALUES ('green pepper',1.25,FALSE);
+INSERT INTO topping (topping_name,additional_price,is_premium) VALUES ('onion',1.25,FALSE);
+INSERT INTO topping (topping_name,additional_price,is_premium) VALUES ('egg',1.25,FALSE);
+INSERT INTO topping (topping_name,additional_price,is_premium) VALUES ('gold leaf',20.00,TRUE);
+INSERT INTO topping (topping_name,additional_price,is_premium) VALUES ('chanterelle mushroom',2.25,TRUE);
+INSERT INTO topping (topping_name,additional_price,is_premium) VALUES ('black truffle',2.25,TRUE);
+INSERT INTO topping (topping_name,additional_price,is_premium) VALUES ('prosciutto',2.25,TRUE);
 
-INSERT INTO topping (topping_name,additional_price,is_premium) VALUES ('prosciutto',2,TRUE);
 
 INSERT INTO pizza_topping (pizza_id,topping_name) VALUES (1,'pepperoni');
+INSERT INTO pizza_topping (pizza_id,topping_name) VALUES (2,'ham');
+INSERT INTO pizza_topping (pizza_id,topping_name) VALUES (2,'olive');
 
 INSERT INTO specialty_topping (specialty_id,topping_name) VALUES (1,'prosciutto');
+INSERT INTO specialty_topping (specialty_id,topping_name) VALUES (1,'pepperoni');
+INSERT INTO specialty_topping (specialty_id,topping_name) VALUES (1,'sausage');
+INSERT INTO specialty_topping (specialty_id,topping_name) VALUES (1,'ham');
+
+INSERT INTO specialty_topping (specialty_id,topping_name) VALUES (2,'olive');
+INSERT INTO specialty_topping (specialty_id,topping_name) VALUES (2,'green pepper');
+INSERT INTO specialty_topping (specialty_id,topping_name) VALUES (2,'onion');
+INSERT INTO specialty_topping (specialty_id,topping_name) VALUES (2,'chanterelle mushroom');
+
+INSERT INTO specialty_topping (specialty_id,topping_name) VALUES (3,'black truffle');
+INSERT INTO specialty_topping (specialty_id,topping_name) VALUES (3,'gold leaf');
+
+INSERT INTO specialty_topping (specialty_id,topping_name) VALUES (4,'egg');
 
 COMMIT TRANSACTION;
 
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS specialty_topping, pizza_topping, topping, current_pizza, specialty_pizza, customer_order, customer, users;
+DROP TABLE IF EXISTS specialty_topping, pizza_topping, topping, pizza, customer_order, specialty_pizza, crust, sauce, size, customer, users;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -20,48 +20,62 @@ CREATE TABLE customer (
     user_id int NOT NULL REFERENCES users(user_id)
 );
 
+CREATE TABLE size (
+    size_name varchar(15) PRIMARY KEY,
+    size_price numeric(6,2)
+);
+
+CREATE TABLE sauce (
+    sauce_name varchar(15) PRIMARY KEY,
+    sauce_price numeric(6,2)
+);
+
+CREATE TABLE crust (
+    crust_name varchar(15) PRIMARY KEY,
+    crust_price numeric(6,2)
+);
+
+CREATE TABLE specialty_pizza (
+    specialty_id SERIAL PRIMARY KEY,
+    pizza_name varchar(50) NOT NULL UNIQUE,
+    pizza_desc varchar(300) NOT NULL
+);
+
 CREATE TABLE customer_order (
     order_id SERIAL PRIMARY KEY,
     total numeric(6,2),
     is_delivery boolean NOT NULL,
     date date NOT NULL,
-    is_active boolean NOT NULL,
     customer_id int NOT NULL REFERENCES customer(customer_id)
 );
 
-CREATE TABLE specialty_pizza (
-    specialty_id SERIAL PRIMARY KEY,
-    price numeric(4,2) NOT NULL,
-    pizza_name varchar(50) NOT NULL UNIQUE
-);
-
-CREATE TABLE current_pizza (
+CREATE TABLE pizza (
     pizza_id SERIAL PRIMARY KEY,
+    additional_instructions varchar(500),
+    size_name varchar(15) DEFAULT('LARGE') NOT NULL REFERENCES size(size_name) ,
+    sauce_name varchar(15)DEFAULT('No Sauce') NOT NULL REFERENCES sauce(sauce_name),
+    crust_name varchar(15) DEFAULT('Hand Tossed') NOT NULL REFERENCES crust(crust_name),
     order_id int NOT NULL REFERENCES customer_order(order_id),
-    size varchar(10) DEFAULT('Large') NOT NULL ,
-    pizza_name varchar(50) NOT NULL UNIQUE,
-    sauce varchar(10),
-    crust varchar(15) DEFAULT('Hand Tossed') NOT NULL,
-    price numeric(4,2) NOT NULL,
-    specialty_id int NOT NULL REFERENCES specialty_pizza(specialty_id),
-    additional_instructions varchar(500)
+    price numeric(6,2) NOT NULL,
+    order_status varchar(10) DEFAULT('Pending') NOT NULL,
+    specialty_id int REFERENCES specialty_pizza(specialty_id)
 );
 
 CREATE TABLE topping (
-    topping_name varchar(15) PRIMARY KEY,
-    additional_price numeric(3,2),
+    topping_name varchar(50) PRIMARY KEY,
+    additional_price numeric(6,2),
     is_premium boolean NOT NULL
 );
 
 CREATE TABLE pizza_topping (
-    pizza_id int NOT NULL REFERENCES current_pizza(pizza_id),
-    topping_name varchar(15) NOT NULL REFERENCES topping(topping_name),
+    pizza_id int NOT NULL REFERENCES pizza(pizza_id),
+    topping_name varchar(50) NOT NULL REFERENCES topping(topping_name),
     CONSTRAINT pk_pizza_topping PRIMARY KEY (pizza_id, topping_name)
 );
 
 CREATE TABLE specialty_topping (
     specialty_id int NOT NULL REFERENCES specialty_pizza(specialty_id),
-    topping_name varchar(15) NOT NULL REFERENCES topping(topping_name)
+    topping_name varchar(50) NOT NULL REFERENCES topping(topping_name)
 );
 
 COMMIT TRANSACTION;
