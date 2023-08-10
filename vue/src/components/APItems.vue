4c3dcf859ee7eaf6c8dca2da0b580e7058405e34
Daniele Marsico <danymarsico@gmail.com>
2023-08-10 10:39:01 -0400

    Changes the APItems component to add grid

--- vue/src/components/APItems.vue
+++ vue/src/components/APItems.vue
@@ -7,6 +7,31 @@
             <td id="checkbox1"> <button>Unavailable</button> </td>
             <td id="checkbox2"> <button>Delete</button> </td>
           </tr>
+          <tr>
+            <td id="item"> <button>Item</button> </td>
+            <td id="checkbox1"> <button>Unavailable</button> </td>
+            <td id="checkbox2"> <button>Delete</button> </td>
+          </tr>
+          <tr>
+            <td id="item"> <button>Item</button> </td>
+            <td id="checkbox1"> <button>Unavailable</button> </td>
+            <td id="checkbox2"> <button>Delete</button> </td>
+          </tr>
+          <tr>
+            <td id="item"> <button>Item</button> </td>
+            <td id="checkbox1"> <button>Unavailable</button> </td>
+            <td id="checkbox2"> <button>Delete</button> </td>
+          </tr>
+          <tr>
+            <td id="item"> <button>Item</button> </td>
+            <td id="checkbox1"> <button>Unavailable</button> </td>
+            <td id="checkbox2"> <button>Delete</button> </td>
+          </tr>
+          <tr>
+            <td id="item"> <button>Item</button> </td>
+            <td id="checkbox1"> <button>Unavailable</button> </td>
+            <td id="checkbox2"> <button>Delete</button> </td>
+          </tr>
         </tbody>
       </table>
   </div>
@@ -21,22 +46,27 @@ export default {
 <style>
 #itemList {
   display: flex;
-  border-bottom: solid 1px #75E8E7;
 }
 
 tr {
   width: 80vw;
   display: flex;
   justify-content: flex-start;
+  border: solid 1px #75E8E7;
+  border-radius: 3px;
+  margin-bottom: 5px;
   
 }
 
 #item {
   margin-left: 32vw;
+  
 }
 
 #checkbox1 {
   margin-left: 25vw;
+  border-left: solid 1px #75E8E7;
+  padding-left: 22px;
 }
 
 #checkbox2 {
