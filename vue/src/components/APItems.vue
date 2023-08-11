79ca1ce6c96707825144f634c1620fd1e0cdad5c
Anastassia Bowers <anastassia.bowers@gmail.com>
2023-08-11 13:45:10 -0700

    Item button toggle functionality

--- vue/src/components/APItems.vue
+++ vue/src/components/APItems.vue
@@ -1,51 +1,58 @@
 <template>
   <div>
-      <table id="itemList">
-        <tbody>
-          <tr>
-            <td id="item"> <h2>Pepperoni</h2> </td>
-            <td id="checkbox1"> <button>Unavailable</button> </td>
-            <td id="checkbox2"> <button>Delete</button> </td>
-          </tr>
-          <tr>
-            <td id="item"> <h2>Sausage</h2> </td>
-            <td id="checkbox1"> <button>Unavailable</button> </td>
-            <td id="checkbox2"> <button>Delete</button> </td>
-          </tr>
-          <tr>
-            <td id="item"> <h2>Ham</h2> </td>
-            <td id="checkbox1"> <button>Unavailable</button> </td>
-            <td id="checkbox2"> <button>Delete</button> </td>
-          </tr>
-          <tr>
-            <td id="item"> <h2>Olive</h2> </td>
-            <td id="checkbox1"> <button>Unavailable</button> </td>
-            <td id="checkbox2"> <button>Delete</button> </td>
-          </tr>
-          <tr>
-            <td id="item"> <h2>Green Pepper</h2> </td>
-            <td id="checkbox1"> <button>Unavailable</button> </td>
-            <td id="checkbox2"> <button>Delete</button> </td>
-          </tr>
-          <tr>
-            <td id="item"> <h2>Onion</h2> </td>
-            <td id="checkbox1"> <button>Unavailable</button> </td>
-            <td id="checkbox2"> <button>Delete</button> </td>
-          </tr>
-        </tbody>
-      </table>
+    <table id="itemList">
+      <tbody>
+        <tr v-for="(item, index) in items" :key="index">
+          <td id="item"><h2>{{item.name}}</h2></td>
+          <td id="checkbox1">
+            <button v-on:click.prevent="toggleAvailability(index)">
+              {{ item.buttonText }}
+            </button>
+          </td>
+          <td id="checkbox2"><button>Delete</button></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
+ data() {
+   return {
+     items: [
+       { name: 'Pepperoni', buttonText: 'Make Unavailble' },
+       { name: 'Sausage', buttonText: 'Make Unavailble' },
+       { name: 'Ham', buttonText: 'Make Unavailble' },
+       { name: 'Olive', buttonText: 'Make Unavailble' },
+       { name: 'Green Pepper', buttonText: 'Make Unavailble' },
+       { name: 'Onion', buttonText: 'Make Unavailble' },
+     ]
+    };
+   },
+    methods: {
+   toggleAvailability(index){
+     for(let i =0; i < this.items.length; i++) {
+       if(index === i) {
+         let item = this.items[i];
+         if(item.buttonText != 'Make Available') {
+           item.buttonText = 'Make Available';
+         } else {
+           item.buttonText= 'Make Unavailable';
+         }
+       }
+     }
 
 
-
+    //  if (this.buttonText === 'Unavailable') {
+    //    this.buttonText = 'Make Available';
+    //  } else {
+    //  this.buttonText = 'Make Unavailable';
+    //  }
+   }
+ }
 }
 </script>
 
 <style>
-
-
 </style>
