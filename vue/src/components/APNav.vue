51bfdb0d935c59d976294e26bc2679770636a13e
Anastassia Bowers <anastassia.bowers@gmail.com>
2023-08-16 11:57:17 -0700

    Admin Nav Bar styling to match customer nav

--- vue/src/components/APNav.vue
+++ vue/src/components/APNav.vue
@@ -1,42 +1,54 @@
 <template>
-<nav id="apNav">
-          <ul id="nav-list">
-              <li><router-link :to="{name: 'AdminHome' }">Menu Items</router-link></li>
-              <li><router-link :to="{name: 'Ingredients' }">Ingredients</router-link></li>
-              <!-- TODO Implement Reports page and add link -->
-              <li><router-link :to="{name: 'Reports'}">Reports</router-link></li>
-              <!-- <li><button id="logoutBtn">Logout</button></li> -->
-              <li><router-link id="logoutBtn" :to="{name: 'Logout'}">Log Out</router-link></li>
-          </ul>
-      </nav>
+  <nav id="apNav">
+    <ul id="nav-list">
+      <li><router-link :to="{ name: 'AdminHome' }">Menu Items</router-link></li>
+      <li>
+        <router-link :to="{ name: 'Ingredients' }">Ingredients</router-link>
+      </li>
+      <!-- TODO Implement Reports page and add link -->
+      <li><router-link :to="{ name: 'Reports' }">Reports</router-link></li>
+      <!-- <li><button id="logoutBtn">Logout</button></li> -->
+      <li>
+        <router-link id="logoutBtn" :to="{ name: 'Logout' }"
+          >Log Out</router-link
+        >
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped>
 #nav-list {
-    width: 90vw ;
-    list-style: none;
-    display: flex;
-    justify-content: space-around;
+  width: 90vw;
+  list-style: none;
+  display: flex;
+  justify-content: space-around;
 }
 #apNav {
-    margin:5px 20px 5px 20px;
-    border: solid 3px #75E8E7;
-    border-radius: 30px;
-    background-color: #DDACF5;
+  margin: 5px 20px 5px 20px;
+  border: solid 3px #75e8e7;
+  border-radius: 30px;
+  background-color: #ddacf5;
 }
-a:link, a:visited {
-    text-decoration: none;
-    color: #27104e;
-    font-weight: bold;
+a:link,
+a:visited {
+  text-decoration: none;
+  color: #64379f;
+  font-family: fantasy;
+  font-weight: lighter;
+  text-shadow: 6px 4px #75e8e7;
+  letter-spacing: 2pt;
+  font-size: 18pt;
 }
-a:hover, a:active {
-    color: #75e8e7;
+a:hover,
+a:active {
+  color: #75e8e7;
+  text-shadow: 6px 4px #64379f;
+  -webkit-text-stroke-width: 1px;
+  -webkit-text-stroke-color: #0089fa;
 }
-
 </style>
