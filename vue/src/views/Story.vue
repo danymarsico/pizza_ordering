dec3a61fdce4302d985630a0ef2a1c62140aac78
David Swanstrom <ifritborndesigns@gmail.com>
2023-08-16 20:26:07 -0600

    updated how the carousel works

--- vue/src/views/Story.vue
+++ vue/src/views/Story.vue
@@ -5,90 +5,21 @@
     <div id="story-main">
       <h1>Backstory</h1>
 
-      <div
-        id="img-container"
-        @mouseover="stopCarousel"
-        @mouseleave="startCarousel"
-      >
-        <img
-          src="../assets/backstory1.jpg"
-          class="pizza-slides"
-          @mouseover="hover = true"
-          @mouseleave="hover = false"
-        />
-        <img
-          src="../assets/backstory2.jpg"
-          class="pizza-slides"
-          @mouseover="hover = true"
-          @mouseleave="hover = false"
-        />
-        <img
-          src="../assets/backstory3.jpg"
-          class="pizza-slides"
-          @mouseover="hover = true"
-          @mouseleave="hover = false"
-        />
-        <img
-          src="../assets/backstory4.jpg"
-          class="pizza-slides"
-          @mouseover="hover = true"
-          @mouseleave="hover = false"
-        />
+      <div  id="img-container">
+        <img src="../assets/backstory4.jpg" id="img-slides" />
 
-        <div id="text-container">
-          <p class="text">
-            In the bustling metropolis of Tech Elevator City, where the fusion
-            of technology and innovation propels the cityscape to new heights, a
-            man named Frank Franklin found himself at a crossroads. Frank had
-            always been captivated by the pulsating rhythms of vaporwave culture
-            and the nostalgic allure of the 80s. Yet, he was trapped in a
-            mundane corporate job that left him yearning for something more. One
-            fateful day, as he sipped on a fluorescent-lit latte in a
-            pastel-colored cafe, he met Luna Echo, a life coach with an aura as
-            vibrant as the city itself.
-          </p>
-          <p class="text">
-            Luna Echo was a beacon of hip wisdom in Tech Elevator City, known
-            for her enigmatic advice and captivating vaporwave-inspired
-            sessions. She sensed Frank's inner turmoil and encouraged him to
-            embrace his passions, to let the retro beats of his heart guide him
-            towards a more fulfilling path. "Live in the moment because
-            everything is a coin flip", she urged, her voice a soothing blend of
-            whispering synths. Inspired by her words, Frank embarked on a
-            journey of self-discovery that would reshape his destiny.
-          </p>
-          <p class="text">
-            Guided by Luna, Frank decided to disrupt the pizza ecosystem that
-            had long grown stagnant. With a sprinkle of stardust and a dash of
-            pixelated creativity, he founded N*A*M*E*L*E*S*S*Π, a pizza joint unlike
-            any other. The restaurant's ambiance was a symphony of holographic
-            posters, glitch art, and holograms playing retro arcade games. Each
-            slice of pizza bore a neon resemblance to its former self, the
-            flavors a fusion of classic recipes and futuristic culinary
-            techniques.
-          </p>
-          <p class="text">
-            As word spread through Tech Elevator City about the enigmatic
-            pizzeria, Frank found himself at the forefront of a cultural
-            revolution. N*A*M*E*L*E*S*S*Π became a haven for the city's avant-garde, a
-            place where those seeking a slice of the past and a bite of the
-            future could come together in harmonious satisfaction. Frank's
-            journey from a stifled corporate drone to a pioneer of gastronomical
-            artistry was a testament to the power of embracing one's true
-            passions, and he owed it all to Luna Echo's transcendent wisdom.
-          </p>
-
-          <div style="text-align: center"><p v-if="hover">Paused</p></div>
+        <div >
+          <p id="text-container"></p>
         </div>
       </div>
-      <div id="slide-buttons">
+      <!-- <div id="slide-buttons">
         <button class="button" @click="prevSlide">&#10094;</button>
-        <input type="radio" name="" id="">
-        <input type="radio" name="" id="">
-        <input type="radio" name="" id="">
-        <input type="radio" name="" id="">
+        <input type="radio" name="" id="" />
+        <input type="radio" name="" id="" />
+        <input type="radio" name="" id="" />
+        <input type="radio" name="" id="" />
         <button class="button" @click="nextSlide">&#10095;</button>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -97,7 +28,7 @@
 import Header from "../components/Header.vue";
 import CustNav from "../components/CustNav.vue";
 
-let slideIndex = 0;
+let arrayIndex = 0;
 
 export default {
   components: {
@@ -111,48 +42,63 @@ export default {
   },
   methods: {
     carousel() {
-      let slides = document.getElementsByClassName("pizza-slides");
-      let text = document.getElementsByClassName("text");
+      let imgArray = [
+        "/img/backstory1.b552cb31.jpg",
+        "/img/backstory2.7e33b4d5.jpg",
+        "/img/backstory3.4e786624.jpg",
+        "/img/backstory4.ddedeaaa.jpg",
+      ];
+      let textArray = [
+        `In the bustling metropolis of Tech Elevator City, where the fusion
+            of technology and innovation propels the cityscape to new heights, a
+            man named Frank Franklin found himself at a crossroads. Frank had
+            always been captivated by the pulsating rhythms of vaporwave culture
+            and the nostalgic allure of the 80s. Yet, he was trapped in a
+            mundane corporate job that left him yearning for something more. One
+            fateful day, as he sipped on a fluorescent-lit latte in a
+            pastel-colored cafe, he met Luna Echo, a life coach with an aura as
+            vibrant as the city itself.`,
+        `Luna Echo was a beacon of hip wisdom in Tech Elevator City, known
+            for her enigmatic advice and captivating vaporwave-inspired
+            sessions. She sensed Frank's inner turmoil and encouraged him to
+            embrace his passions, to let the retro beats of his heart guide him
+            towards a more fulfilling path. "Live in the moment because
+            everything is a coin flip", she urged, her voice a soothing blend of
+            whispering synths. Inspired by her words, Frank embarked on a
+            journey of self-discovery that would reshape his destiny.`,
+        `Guided by Luna, Frank decided to disrupt the pizza ecosystem that
+            had long grown stagnant. With a sprinkle of stardust and a dash of
+            pixelated creativity, he founded N*A*M*E*L*E*S*S*Π, a pizza joint
+            unlike any other. The restaurant's ambiance was a symphony of
+            holographic posters, glitch art, and holograms playing retro arcade
+            games. Each slice of pizza bore a neon resemblance to its former
+            self, the flavors a fusion of classic recipes and futuristic
+            culinary techniques.`,
+        `As word spread through Tech Elevator City about the enigmatic
+            pizzeria, Frank found himself at the forefront of a cultural
+            revolution. N*A*M*E*L*E*S*S*Π became a haven for the city's
+            avant-garde, a place where those seeking a slice of the past and a
+            bite of the future could come together in harmonious satisfaction.
+            Frank's journey from a stifled corporate drone to a pioneer of
+            gastronomical artistry was a testament to the power of embracing
+            one's true passions, and he owed it all to Luna Echo's transcendent
+            wisdom.`,
+      ];
+      document.getElementById("img-slides").src = imgArray[arrayIndex];
+      document.getElementById("text-container").innerHTML = textArray[arrayIndex];
+      arrayIndex++;
 
-      for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-        text[i].style.display = "none";
+      if(arrayIndex > imgArray.length -1){
+        arrayIndex = 0;
       }
 
-      slideIndex++;
-
-      if (slideIndex > slides.length) {
-        slideIndex = 1;
-      }
-      if (slideIndex < 1) {
-        slideIndex = slides.length;
-      }
-
-      slides[slideIndex - 1].style.display = "block";
-      text[slideIndex - 1].style.display = "block";
     },
-    stopCarousel() {
-      clearInterval();
-      console.log("hover active");
-    },
-    startCarousel() {
-      clearInterval();
-      setInterval(this.carousel, 8000);
-      console.log("hover inactive");
-    },
-    nextSlide() {
-      clearInterval();
-      slideIndex++;
-
-      console.log("button hit");
-    },
-    prevSlide() {
-      slideIndex--;
-    },
+    
   },
   mounted: function () {
     this.carousel();
-    this.startCarousel();
+    setInterval(this.carousel, 4000)
+    
   },
 };
 </script>
@@ -219,9 +165,8 @@ p {
 }
 .button {
   height: 30px;
-
 }
-#slide-buttons{
+#slide-buttons {
   display: flex;
   justify-content: space-evenly;
   width: 50vw;
