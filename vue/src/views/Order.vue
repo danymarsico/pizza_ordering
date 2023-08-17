ed90f4c469eef77ee04880da4f1dd08a212f62e1
Marina Sanchez[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C <marina8829@gmail.com>
2023-08-16 17:21:05 -0700

    Order Now page: Updated background image on the Select Sauce and Additional Instructions containers - purple waves

--- vue/src/views/Order.vue
+++ vue/src/views/Order.vue
@@ -377,8 +377,8 @@ export default {
   border-radius: 15px;
   border: solid 3px #75e8e7;
   padding-bottom: 23px;
-  background-image: url("../assets/order-backgroundimg2.jpg");
-  background-size: 25%;
+  background-image: url("../assets/order-backgroundimg5.jpg");
+  background-size: 15%;
 }
 h2 {
   font-size: 30px;
@@ -458,8 +458,8 @@ h2 {
   border-radius: 15px;
   border: solid 3px #75e8e7;
   flex-grow: 1;
-  background-image: url("../assets/order-backgroundimg2.jpg");
-  background-size: 25%;
+  background-image: url("../assets/order-backgroundimg5.jpg");
+  background-size: 15%;
 }
 #instructions label {
   font-size: 28px;
